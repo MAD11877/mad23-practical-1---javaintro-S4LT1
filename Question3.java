553882c9e3883d02a4e7557133917dcc58a13b1b
S4LT1 <130967015+S4LT1@users.noreply.github.com>
2023-04-24 12:02:07 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,10 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("Enter an integer: ");
+    int integer = in.nextInt();
+
+    int result = integer*integer;
+    System.out.println("Result: ", result);
   }
 }
